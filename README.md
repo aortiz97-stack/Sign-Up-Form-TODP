0a3b17459dc4785bef3d7b2f9a4c9b7b2a2819a9
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-19 16:40:50 -0600

    Add messages for password verification while user types in password

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Sign-up-form project from The Odin Project
 Puppet image credited to Joseph Pelling, Becky Sloan, & Baker Terry: https://www.imdb.com/title/tt14932528/?ref_=tt_mv_close
 
 Background Image by <a href="https://pixabay.com/users/chenspec-7784448/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5731623">Chen</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5731623">Pixabay</a>
+
+Password validation code: Credit to w3schools.com, https://www.w3schools.com/howto/howto_js_password_validation.asp
--- form-js.js
+++ form-js.js
@@ -9,11 +9,13 @@ function addMessageHTML(e){
         messageDiv.appendChild(messageOpening);
 
         const messageList = ["A lowercase letter", "A capital (uppercase) letter", "A number", "Minimum of 8 characters"]
+        const idList = ["letter", "capital", "number", "min"]
 
         for (let i = 0; i < messageList.length; i++){
             let p = document.createElement("p");
             p.classList.add("invalid");
             p.textContent = messageList[i];
+            p.id = idList[i];
             messageDiv.appendChild(p);
         }
 
@@ -29,4 +31,46 @@ function deleteMessageHTML(e){
 passwordInputs.forEach((input)=>{
     input.addEventListener("focus", addMessageHTML);
     input.addEventListener("blur", deleteMessageHTML);
+
+    input.onkeyup = function() {
+        function invalidToValid(id){
+            let message = document.getElementById(id);
+            message.classList.remove("invalid");
+            message.classList.add("valid");
+        }
+
+        function validToInvalid(id){
+            let message = document.getElementById(id);
+            message.classList.remove("valid");
+            message.classList.add("invalid");
+        }
+        let lowerCaseLetters = /[a-z]/g;
+        let upperCaseLetters = /[A-Z]/g;
+        let numberValidation = /[0-9]/g;
+        if (input.value.match(lowerCaseLetters)){
+            invalidToValid("letter");
+        }
+        else{
+            validToInvalid("letter");
+        }
+        if (input.value.match(upperCaseLetters)){
+            invalidToValid("capital");
+        }
+        else{
+            validToInvalid("capital")
+        }
+        if (input.value.match(numberValidation)){
+            invalidToValid("number");
+        }
+        else{
+            validToInvalid("number")
+        }
+        if (input.value.length >= 8){
+            invalidToValid("min");
+        }
+        else{
+            validToInvalid("min");
+        }
+    }
 })
+
